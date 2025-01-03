8c41be7c0af709c042f823591e4c870f00bbdcb2
kammeraad <70642817+kammeraad@users.noreply.github.com>
2025-01-03 14:46:52 +0100

    Update streamlit_app.py
    add pd df

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,12 @@ st.write('Chosen name:', name_on_order)
 # Display Ingredients
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+                                                                      
 # Menu
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -39,7 +42,10 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-   
+
+    
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
     my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order)
         values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
